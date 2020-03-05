fdbad2a9fa6f8a09a6458becc576fff135feca34
Kevin Bastoul <kaycbas@gmail.com>
2020-03-05 17:01:25 -0500

    add hello world

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ const port = process.env.PORT || 3000;
 
 app.listen(port);
 
-console.log("Hello World!");
+//console.log("Hello World!");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+
+
 
 module.exports = app;
