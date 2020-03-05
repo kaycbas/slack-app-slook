0e0df15a6359dbef44a0db047cf842b3be85aec6
Kevin Bastoul <kevinbastoul@Kevins-MacBook-Pro-2.local>
2020-03-05 16:00:28 -0500

    Push initial slook app repo to git

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
