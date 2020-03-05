dcc050dc4e79f247886019dfd03e8a9c1007a138
Kevin Bastoul <kaycbas@gmail.com>
2020-03-05 16:47:34 -0500

    set up proj with mlab and some other configs

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 // const ejs = require("ejs");
+
+mongoose.connect("process.env.MONGODB_URI || mongodb://localhost:27017/slookDB", { useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Test 2
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+module.exports = app;
