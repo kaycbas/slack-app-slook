9b97f4e1e742ab893a7e1e0623e0b31ed03302dd
Kevin Bastoul <kaycbas@gmail.com>
2020-03-05 17:49:29 -0500

    added mLab connection uri user and pass - take 2

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodb: {
-    URI: "//kaycbas:mJuChEr94@ds137102.mlab.com:37102/heroku_ffl3g89w"
+    URI: "mongodb://kaycbas:mJuChEr94@ds137102.mlab.com:37102/heroku_ffl3g89w"
   }
 };
