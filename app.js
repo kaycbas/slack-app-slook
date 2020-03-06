f0df9ea4574b81f2a10030945b4bb91626972bb0
Kevin Bastoul <kaycbas@gmail.com>
2020-03-06 10:10:33 -0500

    added slackbot functionality

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require('https');
-const request = require('request')
+const request = require('request');
+const axios = require('axios');
+const SlackBot = require('slackbots');
 const mongoose = require('mongoose');
 // const ejs = require("ejs");
+
+const bot = new SlackBot({
+  token: 'xoxb-982834467216-986730882679-UklOiV9zT7l5Z5ySrZLYzq9G',
+  name: 'slook'
+});
 
 //mongoose.connect("credentials.mongodb.URI || mongodb://localhost:27017/slookDB", { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect("mongodb://kaycbas:mJuChEr94@ds137102.mlab.com:37102/heroku_ffl3g89w");
@@ -20,29 +27,47 @@ app.listen(port);
 
 //console.log("Hello World!");
 
-app.get("/", (req, res) => {
-  res.write("Hello World!");
+// SlackBot instructions
+bot.on('start', () => {
+  var params = {
+    icon_emoji: ':cat:'
+  };
 
-  request.post('https://hooks.slack.com/services/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', {
-    json: {
-      todo: 'Buy the milk'
-    }
-  }, (error, res, body) => {
-    if (error) {
-      console.error(error)
-      return
-    }
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(body)
-  });
+  bot.postMessageToChannel('general', 'meow!', params);
+});
+
+app.get("/", (req, res) => {
+  res.write("Hello World!\n");
+
+  // request.post('https://hooks.slack.com/services/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', {
+  //   json: {
+  //     todo: 'Buy the milk'
+  //   }
+  // }, (error, res, body) => {
+  //   if (error) {
+  //     console.error(error)
+  //     return
+  //   }
+  //   console.log(`statusCode: ${res.statusCode}`)
+  //   console.log(body)
+  // });
 
   res.write("Hello World 2!");
   res.send();
 });
 
-
-
-
+// request.post('https://hooks.slack.com/services/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', {
+//   json: {
+//     "text":"Hello, World!"
+//   }
+// }, (error, res, body) => {
+//   if (error) {
+//     console.error(error)
+//     return
+//   }
+//   console.log(`statusCode: ${res.statusCode}`)
+//   console.log(body)
+// });
 
 
 module.exports = app;
